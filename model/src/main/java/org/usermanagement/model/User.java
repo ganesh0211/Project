3052b714916d625f13134a654cc4df0415efb02e
ganesh0211 <ganesh0211@gmail.com>
2016-05-11 16:16:54 +0530

    Plateform model added

---
+++ model/src/main/java/org/usermanagement/model/User.java
@@ -0,0 +1,12 @@
+package org.usermanagement.model;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ganesh
+ * Date: 11/5/16
+ * Time: 3:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+}
